c38ff4835304f76cba13a1fd5e6820ef9215faf2
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-03-06 18:36:53 +0700

    htd

---
+++ jslesson1/scr.js
@@ -0,0 +1,11 @@
+let NAME = "Evgeniy";
+let OCCUPATION = "Jewellry master";
+let YEAR_OF_BIRTH = 1994;
+let CURRENT_YEAR = 2023;
+let AGE = CURRENT_YEAR - YEAR_OF_BIRTH;
+
+let STORY = `My name is ${NAME}, my old ${AGE} year, and i am  ${OCCUPATION}`;
+console.log(NAME);
+console.log(OCCUPATION);
+console.log(AGE);
+console.log(STORY);
